04043736103fecd8eb17c604be262d130ac85681
kilik42 <marvinevins@yahoo.com>
2025-11-24 15:36:36 -0600

    working with orders layout

--- src/app/shop/orders/_layout.tsx
+++ src/app/shop/orders/_layout.tsx
@@ -11,7 +11,7 @@ export default function OrdersLayout() {
                 headerShown: false,
                 title: 'Orders'
                 }} />
-        </Stack>>
-    );
+        </Stack>
+    )
 }       
 
